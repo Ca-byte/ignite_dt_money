e8b7b0f1f690125ee4a8f42a5d7456d8bdb79b80
Caroline Vieira <carolinesanvieira@gmail.com>
2022-12-08 21:07:51 +0100

    feat: Transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
   return(
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Software development</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 23.000,00
+                </PriceHighlight>
+              </td>
+              <td>Sales</td>
+              <td>12/02/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  R$ 28,00
+                </PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>09/02/2023</td>
+            </tr>
+            
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
