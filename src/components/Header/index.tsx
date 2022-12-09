e04cfb510ec5ee7bed0fc4599fdde92e2390134f
Caroline Vieira <carolinesanvieira@gmail.com>
2022-12-09 15:42:17 +0100

    feat: Create modal, added Radix lib

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,25 @@
 import { HeaderContainer, HeaderContent, NewTransactionBtn } from "./styles";
 import logoImg from '../../assets/ignite-dt-money-logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header(){
 	return(
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoImg} alt=""/>
-				<NewTransactionBtn>New Transaction</NewTransactionBtn>
+				<Dialog.Root>
+          <Dialog.Trigger asChild>
+					  <NewTransactionBtn>New Transaction</NewTransactionBtn>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>New Transaction</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
 	)
