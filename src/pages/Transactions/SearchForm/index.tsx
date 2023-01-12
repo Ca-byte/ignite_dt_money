ccf5f05ead4ffd8b42ca918581a7e2da5435dafb
Caroline Vieira <carolinesanvieira@gmail.com>
2023-01-12 11:51:31 +0100

    feat: Performance - Applying memo concept

--- src/pages/Transactions/SearchForm/index.tsx
+++ src/pages/Transactions/SearchForm/index.tsx
@@ -6,6 +6,23 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../contexts/TransactionsContexts'
 import { useContextSelector } from 'use-context-selector'
 
+/**
+ * Why a component is render
+ * - Hooks changed
+ * - Props changed
+ * - Parent rerendered
+ *
+ * What is the renderization flow
+ * 1. React create HTML interface component
+ * 2. Compare step 1 with the previous HTML version
+ * 3. If something changed it is update and the new HTML is shown
+ *
+ * Memo:
+ * 0. Hooks changed, props changed (deep comparison)
+ * 0.1 Compare the previous Hooks and props vesion
+ * 0.2 If something changed it will allow the new renderization.
+ */
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
