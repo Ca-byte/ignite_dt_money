632ea286f3dd149d178085d700d4aef03c45c17d
Caroline Vieira <carolinesanvieira@gmail.com>
2023-01-13 22:19:33 +0100

    fix: Performance - Added return to useMemo hook

--- src/hooks/useSumary.ts
+++ src/hooks/useSumary.ts
@@ -7,7 +7,7 @@ export function useSumary() {
     return context.transactions
   })
   const summary = useMemo(() => {
-    transactions.reduce(
+    return transactions.reduce(
       (acc, transaction) => {
         if (transaction.type === 'income') {
           acc.income += transaction.price
